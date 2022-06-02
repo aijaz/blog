6a5f859e703bc6490ca40b583427a89a23f685e4
Aijaz Ansari <aijaz@users.noreply.github.com>
2022-06-01 20:57:57 -0600

    Not echoing every file name during compression

--- compressFile.sh
+++ compressFile.sh
@@ -6,7 +6,6 @@ set -e
 for FILE in `find output_stage -type f -not -name '*.gz' -not -name '*.gif' -not -name '*.jpg' -not -name '*.png' -not -name '.DS_Store' `; do
   GZFILE=${FILE}.gz
 
-  echo $FILE
   if [ -f "${GZFILE}" ];  then
       if [ "${FILE}" -nt "${GZFILE}" ]; then
       echo "Compressing newer than '$GZFILE'"
